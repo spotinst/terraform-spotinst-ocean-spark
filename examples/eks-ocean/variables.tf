5e8e34ebaa6bb5ba4bfa0e8fd7c56248bb271a43
ImpSy <3097030+ImpSy@users.noreply.github.com>
2022-03-11 15:35:31 +0100

    add exemple for deploying for scratch on EKS (aws)

---
+++ examples/eks-ocean/variables.tf
@@ -0,0 +1,19 @@
+variable "spotinst_token" {
+  type        = string
+  description = "Spot Personal Access token"
+}
+
+variable "spotinst_account" {
+  type        = string
+  description = "Spot account ID"
+}
+
+variable "aws_region" {
+  type        = string
+  description = "EKS cluster region"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = "AWS profile to use"
+}
