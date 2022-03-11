42d21f24c8db54ac6df7c5fb84fd0d92729c6a80
ImpSy <3097030+ImpSy@users.noreply.github.com>
2022-03-11 15:10:04 +0100

    bootstrap module

---
+++ main.tf
@@ -0,0 +1,71 @@
+locals {
+  spot_system_namespace = "spot-system"
+  service_account_name  = "bigdata-deployer"
+  role_binding_name     = "bigdata-deployer-admin"
+}
+
+resource "kubernetes_namespace" "spot-system" {
+  count = var.create_ofas_cluster && var.create_spot_system_namespace ? 1 : 0
+  metadata {
+    name = local.spot_system_namespace
+  }
+}
+
+resource "kubernetes_service_account" "deployer" {
+  count = var.create_ofas_cluster ? 1 : 0
+  metadata {
+    name      = local.service_account_name
+    namespace = local.spot_system_namespace
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "deployer" {
+  count = var.create_ofas_cluster ? 1 : 0
+  metadata {
+    name = local.role_binding_name
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = local.service_account_name
+    namespace = local.spot_system_namespace
+  }
+}
+
+resource "kubernetes_job" "deployer" {
+  count = var.create_ofas_cluster ? 1 : 0
+  metadata {
+    generate_name = "ofas-deploy-"
+    namespace     = local.spot_system_namespace
+  }
+  spec {
+    template {
+      metadata {}
+      spec {
+        container {
+          name              = "deployer"
+          image             = "${var.ofas_deployer_image}:${var.ofas_deployer_tag}"
+          image_pull_policy = var.image_pull_policy
+          args              = ["install", "--create-bootstrap-environment"]
+        }
+        restart_policy       = "Never"
+        service_account_name = local.service_account_name
+      }
+    }
+    ttl_seconds_after_finished = 300
+  }
+  wait_for_completion = true
+  timeouts {
+    create = "5m"
+    update = "2m"
+  }
+  depends_on = [
+    kubernetes_namespace.spot-system,
+    kubernetes_service_account.deployer,
+    kubernetes_cluster_role_binding.deployer,
+  ]
+}
