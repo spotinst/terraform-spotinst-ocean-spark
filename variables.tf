84221f4e1f5f8afbb78b414082c0d5c2ddccfe68
ImpSy <3097030+ImpSy@users.noreply.github.com>
2022-03-16 17:47:05 +0100

    modify variables before initial release

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
-variable "create_ofas_cluster" {
+variable "create_cluster" {
   type        = bool
-  description = "Specifies whether to create an Ocean for Apache Spark cluster"
+  description = "Controls whether the Ocean for Apache Spark cluster should be created (it affects all resources)"
   default     = true
 }
 
-variable "ofas_deployer_image" {
+variable "deployer_image" {
   type        = string
   description = "Specifies the Docker image name used in the deployer Job"
   default     = "public.ecr.aws/f4k1p1n4/bigdata-deployer"
 }
 
-variable "ofas_deployer_tag" {
+variable "deployer_tag" {
   type        = string
   description = "Specifies the Docker image tag used in the deployer Job"
   default     = "main"
@@ -20,11 +20,4 @@ variable "image_pull_policy" {
   type        = string
   description = "Specifies the image pull policy (one of: Always, Never, IfNotPresent)"
   default     = "Always"
-}
-
-
-variable "create_spot_system_namespace" {
-  type        = bool
-  description = "Specifies whether to create a namespace for the Spot components"
-  default     = true
 }
