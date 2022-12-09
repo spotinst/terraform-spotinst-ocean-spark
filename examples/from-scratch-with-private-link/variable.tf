3606d4c20257f6a54d1d2e661fea89a748563252
Z4ck404 <zakaria.elbazi@netapp.com>
2022-12-09 14:53:57 +0100

    [exmaple] add private-link config exmaple

---
+++ examples/from-scratch-with-private-link/variable.tf
@@ -0,0 +1,35 @@
+variable "spotinst_token" {
+  type = string
+}
+
+variable "spotinst_account" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
+
+variable "aws_profile" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "vpc_name" {
+  type = string
+}
+
+variable "vpc_cidr" {
+  type = string
+}
+
+variable "target_group" {
+  type = map(string)
+  default = {
+    "Protocol" = "TCP"
+    "Port"     = "443"
+  }
+}
