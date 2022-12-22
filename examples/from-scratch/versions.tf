2ec4917aed5f6b2e3541e81f2a104ccca2f22d0c
Z4ck404 <zakaria.elbazi@netapp.com>
2022-12-22 10:40:32 +0100

    update the spotinst provider in all the examples

--- examples/from-scratch/versions.tf
+++ examples/from-scratch/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     spotinst = {
       source  = "spotinst/spotinst"
-      version = "~> 1.84"
+      version = "~> 1.90"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
