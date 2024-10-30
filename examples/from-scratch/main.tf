7436dd07898eb37a55c57b69dcabab2266065cb8
Z4ck404 <zakaria.elbazi@netapp.com>
2024-10-30 20:11:32 +0100

    [examples/from-scratch] use ocean controller v2

--- examples/from-scratch/main.tf
+++ examples/from-scratch/main.tf
@@ -192,10 +192,12 @@ module "ocean-aws-k8s" {
   }
 }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.this.token
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
 }
 
 module "ocean-controller" {
